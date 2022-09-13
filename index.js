87990860541e71fc8799694e40a6594d3f6dbeb1
Roni-ai <113049933+ronbot-ai@users.noreply.github.com>
2022-09-13 20:23:00 +0300

    add Js scripts

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+let dragItem = document.getElementById("dragItem");
+            dragItem.addEventListener("dragstart", e=>{
+                e.dataTransfer.setData("text/plain","drag me now");
+            });
 
+
+                for(let dropBox of document.querySelectorAll(".dropBox")){
+                    // dragded item is getting swapped
+                    dropBox.addEventListener("dragover", e=>{
+                    e.preventDefault();
+                    dropBox.classList.add("itemSwap");
+                    });
+
+                    // the item  is no longer over the dropBox
+                    dropBox.addEventListener("dragleave", e=>{
+                    dropBox.classList.remove("itemSwap");
+                    });
+                    //item being swapped 
+                    dropBox.addEventListener("drop",e=>{
+                        e.preventDefault();
+                        dropBox.appendChild(dragItem)
+                    
+                       
+                    });
+
+                }
